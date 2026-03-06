d094eab0064b91ed3b76fa7cd2f41368015b99c6
ZKB213 <zakaria7bennaceur@gmail.com>
2026-03-06 15:13:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
+import os
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-import os
 TOKEN = os.getenv(8099010522:AAGw-7vF2mvV9LNgNaMkn8wnO4GJQI-uO0E)
 
 FILE = "argent.json"
@@ -12,14 +12,14 @@ def charger():
         with open(FILE) as f:
             return json.load(f)
     except:
-        return {"solde":700}
+        return {"solde":1000}
 
 def sauver(data):
     with open(FILE,"w") as f:
         json.dump(data,f)
 
 data = charger()
-etat = None
+etat_users = {}
 
 keyboard = [["Gain","Perte"],["Solde"]]
 
@@ -31,34 +31,45 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global etat
 
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.first_name
     texte = update.message.text
 
     if texte == "Gain":
-        etat = "gain"
-        await update.message.reply_text("Entre le montant gagné")
+        etat_users[user_id] = "gain"
+        await update.message.reply_text("Entre le montant du gain")
 
     elif texte == "Perte":
-        etat = "perte"
-        await update.message.reply_text("Entre le montant perdu")
+        etat_users[user_id] = "perte"
+        await update.message.reply_text("Entre le montant de la perte")
 
     elif texte == "Solde":
-        await update.message.reply_text(f"💰 Ton solde : {data['solde']}€")
+        await update.message.reply_text(f"💰 Solde actuel : {data['solde']}€")
 
     else:
         try:
             montant = float(texte)
 
+            etat = etat_users.get(user_id)
+
             if etat == "gain":
                 data["solde"] += montant
+                action = "gain"
 
             elif etat == "perte":
                 data["solde"] -= montant
+                action = "perte"
+
+            else:
+                return
 
             sauver(data)
 
-            await update.message.reply_text(f"✅ Nouveau solde : {data['solde']}€")
+            await update.message.reply_text(
+                f"👤 {user_name} a ajouté une {action} de {montant}€\n"
+                f"💰 Nouveau solde : {data['solde']}€"
+            )
 
         except:
             await update.message.reply_text("Entre un nombre valide")
